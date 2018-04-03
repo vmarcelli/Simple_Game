14c0fa5b8269b6f104e80d27ee0bceef9866f2ed
Valerio <vpmarcelli@gmail.com>
2018-04-03 15:49:29 -0700

    Added NPCs to table
    Added the Farmer

--- Data/NPC_Table.cpp
+++ Data/NPC_Table.cpp
@@ -16,11 +16,20 @@ NPC_Table::NPC_Table() {
 }
 
 void NPC_Table::createTable() {
+	/* TO DO: Finish up the NPC table, at least a basics */
 	//Goblin
 	int gobo[7] = { 3, 2, 2, 2, 2, 2, 2 };
 	table[0].setNameStats("Goblin", gobo);
 	table[0].setWeapon(Weps.getWeapon(0));
 	table[0].setArmor(Arms.getArmor(0));
+
+	//Farmer
+	int farm[7] = { 1, 1, 1, 1, 1, 1, 1 };
+	table[1].setNameStats("Farmer", farm);
+	table[1].setWeapon(Weps.getWeapon(0));
+	table[1].setArmor(Arms.getArmor(0));
+
+	//Zombie
 }
 
 Character NPC_Table::getNPC(int index) {
